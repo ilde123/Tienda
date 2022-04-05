f834fe8c05c976b970275d5f10947f81b3b7449b
ilde123 <ilde.noguerol@gmail.com>
2022-04-05 15:53:58 +0200

    Update modalCambio.js

--- js/modalCambio/modalCambio.js
+++ js/modalCambio/modalCambio.js
@@ -1,83 +1,84 @@
+const INPUT_ENTREGA = $('#entregaModalCambio');
+const BOTON_ACEPTAR = $('#btnAceptarCambio');
+const MODAL_CAMBIO = $('#modalCambio');
+
 // Evento mostrar modal cambio
-$('#modalCambio').on('shown.bs.modal', () => {
-	$('#entregaModalCambio').focus();
-	
+MODAL_CAMBIO.on('shown.bs.modal', () => {
+	INPUT_ENTREGA.focus();
+});
 
-	let totalAPagar = formatNumber(TOTAL.val());
+INPUT_ENTREGA.keyup((e) => {
+	// Tecla intro pulsada
+	if (e.keyCode == 13) {
+		// Disparar evento click
+		BOTON_ACEPTAR.trigger('click');
+	} else { // Ofrecer el cambio
+		let totalAPagar = formatNumber(TOTAL.val());
+		let importe = formatNumber(INPUT_ENTREGA.val());
+		let imputCambio = $('#cambioModalCambio');
 
-	// Mostrar el cambio
-	let inputEntrega = $('#entregaModalCambio');
-	let importe = formatNumber(inputEntrega.val());
-
-	inputEntrega.keyup((e) => {
-		// Tecla intro pulsada
-		if (e.keyCode == 13) {
-			// Disparar evento click
-			$('#btnAceptarCambio').trigger('click');
-		} else { // Ofrecer el cambio
-			let imputCambio = $('#cambioModalCambio');
-
-			importe = formatNumber(inputEntrega.val());
-			inputEntrega.val(numerosDecimalesMostrar(importe));
-
-			// COMPROBAR QUE LA CANTIDAD INSERTADA SUPERA EL TOTAL A PAGAR
-			if (parseFloat(importe) >= parseFloat(totalAPagar)) {
-				let aDevolver = numerosDecimales(importe - totalAPagar);
-				imputCambio.val(numerosDecimalesMostrar(aDevolver));
-			} else {
-				imputCambio.val('');
-			}
-		}
-	});
-
-	// Evento aceptar cambio
-	$('#btnAceptarCambio').click((e) => { 
-		e.preventDefault();
+		importe = formatNumber(INPUT_ENTREGA.val());
+		INPUT_ENTREGA.val(numerosDecimalesMostrar(importe));
 
 		// COMPROBAR QUE LA CANTIDAD INSERTADA SUPERA EL TOTAL A PAGAR
 		if (parseFloat(importe) >= parseFloat(totalAPagar)) {
-			insertarPedido();
+			let aDevolver = numerosDecimales(importe - totalAPagar);
+			imputCambio.val(numerosDecimalesMostrar(aDevolver));
 		} else {
-			let falta = numerosDecimales(Math.abs(importe - totalAPagar));
-			msg(`Faltan ${numerosDecimalesMostrar(falta.toString())} €`, 'rojo');
-		}
-	});
-
-	function insertarPedido() {
-		let datos = '';
-
-		TBODY.children('tr').each((_index, element) => {
-			let fila = $(element);
-			let codigo = fila.find('th input').val();
-			let unidades = fila.find('td.col-unidades input').val();
-			let precio = fila.find('td.col-precio input').val();
-
-			if (codigo != '' || unidades != '' || precio != '') {
-				datos += `codigo[]=${codigo}&unidades[]=${unidades}&precio[]=${precio}&`;
-			}
-		});
-
-		// Borrar el último carácter "&"
-		datos = datos.substring(0, datos.length - 1);
-
-		if (datos != '') {
-			$.post("php/insertarPedido.php", datos,
-				(json) => {
-					if (json.resultado == 'ok') {
-						//msg(json.msg, 'azul');
-						$('#modalCambio').modal('hide');
-						limpiarTabla();
-					} else {
-						msg(json.msg, 'rojo');
-					}
-				},
-				"json"
-			);
+			imputCambio.val('');
 		}
 	}
 });
 
+BOTON_ACEPTAR.click((e) => { // Evento aceptar cambio
+	e.preventDefault();
+
+	let importe = formatNumber(INPUT_ENTREGA.val());
+	let totalAPagar = formatNumber(TOTAL.val());
+
+	// COMPROBAR QUE LA CANTIDAD INSERTADA SUPERA EL TOTAL A PAGAR
+	if (parseFloat(importe) >= parseFloat(totalAPagar)) {
+		insertarPedido();
+	} else {
+		let falta = numerosDecimales(Math.abs(importe - totalAPagar));
+		msg(`Faltan ${numerosDecimalesMostrar(falta.toString())} €`, 'rojo');
+	}
+});
+
+function insertarPedido() {
+	let datos = '';
+
+	TBODY.children('tr').each((_index, element) => {
+		let fila = $(element);
+		let codigo = fila.find('th input').val();
+		let unidades = fila.find('td.col-unidades input').val();
+		let precio = fila.find('td.col-precio input').val();
+
+		if (codigo != '' || unidades != '' || precio != '') {
+			datos += `codigo[]=${codigo}&unidades[]=${unidades}&precio[]=${precio}&`;
+		}
+	});
+
+	// Borrar el último carácter "&"
+	datos = datos.slice(0, -1);
+
+	if (datos != '') {
+		$.post("php/insertarPedido.php", datos,
+			(json) => {
+				if (json.resultado == 'ok') {
+					//msg(json.msg, 'azul');
+					MODAL_CAMBIO.modal('hide');
+					limpiarTabla();
+				} else {
+					msg(json.msg, 'rojo');
+				}
+			},
+			"json"
+		);
+	}
+}
+
 // Evento ocultar modal cambio
-$('#modalCambio').on('show.bs.modal', () => {
-	$('#modalCambio input').val('');
+MODAL_CAMBIO.on('show.bs.modal', () => {
+	MODAL_CAMBIO.find('input').val('');
 });
