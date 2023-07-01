8237345203208f7ad9a5988d07247fd75b6c3616
ilde123 <ilde.noguerol@gmail.com>
2023-07-01 21:57:03 +0200

    Update getFamilias.php

--- php/getFamilias.php
+++ php/getFamilias.php
@@ -2,18 +2,19 @@
 	require 'datosDB.php';
 
 	$sql = "SELECT familia FROM familia ORDER BY familia;";
-	$resultado = $conexion->query($sql);
-	$n_filas = $resultado->num_rows;
+	if ($resultado = $conexion->query($sql)) {
+		$n_filas = $resultado->num_rows;
 
-	if ($n_filas > 0) {
-		$datos = [];
-	
-		while ($fila = $resultado->fetch_assoc()) {
-			$datos[] = $fila;
+		if ($n_filas > 0) {
+			$datos = [];
+		
+			while ($fila = $resultado->fetch_assoc()) {
+				$datos[] = $fila;
+			}
 		}
-	}
 
-	echo json_encode($datos);
+		echo json_encode($datos);
+	}
 
 	$conexion->close();
 ?>
