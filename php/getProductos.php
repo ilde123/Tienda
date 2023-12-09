6a4b19937ac3b76c33b78eb5bafdf66bcc99970f
ilde123 <ilde.noguerol@gmail.com>
2023-12-09 02:39:16 +0100

    Cambiar lista productos en la tabla de pedidos principal

--- php/getProductos.php
+++ php/getProductos.php
@@ -1,15 +1,35 @@
 <?php
 	require 'datosDB.php';
 
-	$sql = "SELECT codigo, descripcion FROM producto ORDER BY descripcion;";
-	$resultado = $conexion->query($sql);
-	$n_filas = $resultado->num_rows;
+	$n_filas = 0;
+	$datos = [];
 
-	if ($n_filas > 0) {
-		$datos = [];
-	
-		while ($fila = $resultado->fetch_assoc()) {
-			$datos[] = $fila;
+	if (isset($_GET['descripcion'])) {
+		$descrcipcion = trim($_GET['descripcion']);
+
+		$sqlDescripcion = "SELECT codigo, descripcion, url_imagen FROM producto WHERE UPPER(descripcion) LIKE CONCAT('%',?,'%') ORDER BY descripcion;";
+
+		if ($stmt = $conexion->prepare($sqlDescripcion)) {
+			$stmt->bind_param('s', $descrcipcion);
+			// ejecuta sentencias prepradas
+			$stmt->execute();
+
+			$resultado = $stmt->get_result();
+
+			while ($fila = $resultado->fetch_array()) {
+				$datos[] = $fila;
+			}
+		}
+	} else {
+		
+		$sql = "SELECT codigo, descripcion, url_imagen FROM producto ORDER BY descripcion;";
+		$resultado = $conexion->query($sql);
+		$n_filas = $resultado->num_rows;
+		
+		if ($n_filas > 0) {
+			while ($fila = $resultado->fetch_assoc()) {
+				$datos[] = $fila;
+			}
 		}
 	}
 
